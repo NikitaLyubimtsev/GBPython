281e982e3ae3fa92ccf6cbf1b08b2d158c09809a
NikitaAlexandrovich <lyubimtsevn.a@yandex.ru>
2023-03-22 23:56:54 +0300

    Task24

--- Seminar4/Task24.py
+++ Seminar4/Task24.py
@@ -1,21 +1,9 @@
 from random import randint
 
-n = 5
+bushes = [randint(15, 40) for i in range(int(input('Введите количество кустов: ')))]
+# Преобразуем список присоединяя крайние элементы
+full_bushes = bushes[-1:] + bushes + bushes[:1]
+# Суммируемые элементы и ищем максимальный по значению из элемент списка
+max_berry = max([sum(full_bushes[(i - 1) : (i + 2)]) for i in range(1, (len(bushes) + 1))])
 
-bushes = [{i: randint(1, 10)} for i in range(1, n + 1)]
-print(bushes)
-# res_dict = dict()
-# for i in range(n):
-#     if i > 0 and (i + 1) < n:
-#         #print(bushes[(i - 1) : (i + 2)])
-#         res_dict[i + 1] = sum(bushes[(i - 1) : (i + 2)])
-#     elif i == 0:
-#         #print(bushes[:i + 2], bushes[n - 1])
-#         res_dict[i + 1] = sum(bushes[:i + 2]) + bushes[n - 1]
-#     else:
-#         #print(bushes[i - 1:], bushes[0])
-#         res_dict[i + 1] = sum(bushes[i - 1:]) + bushes[0]
-
-# max_berry = max(res_dict.values())
-
-#print('Максимальное количество собираемых ягод:', max_berry)
+print('Максимальное возможное количество ягод:', max_berry)
