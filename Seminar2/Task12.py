25508d69ff71046fd9900140042f375c8b6857a8
NikitaAlexandrovich <Lyubimtsevb.a@yandex.ru>
2023-03-22 13:54:17 +0300

    Task12

---
+++ Seminar2/Task12.py
@@ -0,0 +1,13 @@
+from random import randint
+from math import sqrt
+
+x, y = randint(1, 10), randint(1, 10)
+sum, comp = x + y, x * y
+
+desc = sum * sum - 4 * comp # находим дискриминант
+if desc > 0: 
+    sd = int(sqrt(desc))
+    y = (sum + sd)/2
+    print('X:', int(sum - y), 'Y:', int(y))
+else:
+    print('Ошибка данных, у урованения нет корней!')
