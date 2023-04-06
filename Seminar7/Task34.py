154973e900334a42f93f2443eac75d27079c92aa
NikitaAlexandrovich <LyubimtsevN.A@yandex.ru>
2023-04-06 13:03:30 +0300

    Added Seminar7

---
+++ Seminar7/Task34.py
@@ -0,0 +1,7 @@
+def rifma(chant):
+    st = chant.lower().split()
+    f = lambda x: sum(1 for i in x if i in 'аеёиоуыэюя')
+    tmp = f(st[0])
+    if all([f(i) == tmp for i in st]):
+        return 'Парам пам-пам'
+    return 'Пам парам'
